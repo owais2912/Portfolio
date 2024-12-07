13d2f47378292b133272cb514a952d58afe0e4d0
owais2912 <owaisansari2912@gmail.com>
2024-12-08 02:53:49 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoute';
+import { Analytics } from '@vercel/analytics/next';
 
 const App = () => {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
